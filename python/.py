afed813f833c7e25de1135074fa89c151153655c
Janiclebson da Silva Pereira Filho <clebsonfilho90@gmail.com>
2026-09-23 19:15:41 -0300

    Delete python directory

--- python/.py
+++
@@ -1,9 +0,0 @@
-from math import trunc
-num1 = float(input("Qual o número?"))
-print(f"O número inteiro é: {trunc(num1)}")
-
-from datetime import date
-print(date.today().year)
-
-from datetime import date
-print(date.today())
